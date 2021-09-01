e1ef2ecb23a9b4d2211f026a241cfa10ee5e2e48
Satyam <satyammourya8989@gmail.com>
2021-09-01 12:45:39 +0530

    ip added

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ class Question(db.Model):
     option_3 = db.Column(db.String(1024), nullable=False, unique=False)
     option_4 = db.Column(db.String(1024), nullable=False, unique=False)
     answer   = db.Column(db.Integer(),nullable=False,unique=False)
+class Admin_log(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    ip=db.Column(db.String(20),nullable=False)
 #question=[]
 @app.route('/')
 @app.route('/login')
@@ -29,6 +32,7 @@ def index():
     session["email"]=""
     session["flag"] =0
     session['complete']=False
+    session["access"]=0
     session["questionsa"]=[]
     return render_template('login.html')
 @app.route('/quest',methods=["GET","POST"])
@@ -123,23 +127,86 @@ def submit():
         return redirect('/',code=302)
 @app.route('/admin')
 def adminlog():
+    x = Admin_log.query.all()
+    fl = 0
+    for i in x:
+        if i.ip == request.environ['REMOTE_ADDR']:
+            fl = 1
+            break
+    if fl:
+        return redirect('/admin_check',code=302)
     return render_template("adminlogin.html")
 @app.route('/admin_check',methods=["GET","POST"])
 def admincheck():
     user = request.form.get("username")
     pswd = request.form.get("pswd")
-    if user=="walkover" and pswd=="walkover":
-        session["access"]=1
-        return redirect('/admin-surprise',code=302)
+    x=Admin_log.query.all()
+    fl=0
+    for i in x:
+        if i.ip==request.environ['REMOTE_ADDR']:
+            fl=1
+            break
+    if (user == "walkover" and pswd == "walkover") or fl:
+        session["access"] = 1;
+        user = User.query.all()
+        user.sort(key=lambda x: x.date, reverse=True)
+        mn = []
+        for i in user:
+            d = {}
+
+            d['username'] = i.username
+            d['email_address'] = i.email_address
+            d['marks'] = i.marks
+            d['date'] = i.date
+
+            mn.append(d)
+        # print(len(mn),user)
+        session['user'] = mn
+        session['pages'] = 1
+        session['total'] = -(-len(mn) // 5)
+        if fl==0:
+            use_ip=Admin_log()
+            use_ip.ip=request.environ['REMOTE_ADDR']
+        #session['ip'].append(request.environ['REMOTE_ADDR'])
+        #print(session['ip'])
+        return redirect('/admin-surprise', code=302)
     return "<h1>Access Denied</h1>"
-@app.route('/admin-surprise')
+
+@app.route('/admin-surprise',methods=["GET","POST"])
 def admin():
-    if session["access"]!=1:
+    #print(request.environ['REMOTE_ADDR'], 'a')
+    mn = request.environ['REMOTE_ADDR']
+    #print(mn, session['ip'])
+    x = Admin_log.query.all()
+    fl = 0
+    for i in x:
+        if i.ip == request.environ['REMOTE_ADDR']:
+            fl = 1
+            break
+    if (fl):
+        session['access'] = 1
+    if session["access"] != 1:
         return "<h1>Access Denied</h1>"
-    session["access"]=0
-    user=User.query.all()
-    user.sort(key=lambda x:x.date)
-    #print(user)
-    return render_template("admin.html",user=user)
+    user = session['user']
+    page = session['pages']
+    option = request.args.get("option")
+    try:
+        option = int(option)
+    except:
+        option = 0
+    if (option == 1):
+        page += 1
+    elif (option == 2):
+        page -= 1
+    session['pages'] = page
+    # print(page)
+    flag = 1
+    flag1 = 1
+    if (page == 1):
+        flag1 = 0
+    if (page == session['total']):
+        flag = 0
+    # print(user)
+    return render_template("admin.html", user=user[5 * (page - 1):5 * page], flag=flag, flag1=flag1, page=1)
 if __name__=="__main__":
     app.run(debug=False)
